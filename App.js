af7fea5a61fe308cb68d02419125783f1e5df54a
Collins U <collinsushi@gmail.com>
2022-12-10 00:37:00 +0000

    Flatlist added

--- App.js
+++ App.js
@@ -1,68 +1,136 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
-const App = ()=> {
+const App = () => {
+  const [cars, setCar] = useState([
+    {
+      color: "purple",
+      type: "Minivan",
+      registration: new Date("2017-01-03"),
+      capacity: 7,
+    },
+    {
+      color: "red",
+      type: "Station Wagon",
+      registration: new Date("2020-07-21"),
+      capacity: 5,
+    },
+    {
+      color: "orange",
+      type: "Sienna",
+      registration: new Date("2019-03-10"),
+      capacity: 6,
+    },
+    {
+      color: "Navy Blue",
+      type: "Truck",
+      registration: new Date("2021-11-10"),
+      capacity: 2,
+    },
+    {
+      color: "Purple",
+      type: "Bus",
+      registration: new Date("2018-03-03"),
+      capacity: 8,
+    },
+    {
+      color: "orange",
+      type: "Volkswagon",
+      registration: new Date("2019-03-10"),
+      capacity: 10,
+    },
+    {
+      color: "Navy Blue",
+      type: "Honda",
+      registration: new Date("2021-11-10"),
+      capacity: 12,
+    },
+    {
+      color: "Purple",
+      type: "Bike",
+      registration: new Date("2018-03-03"),
+      capacity: 9,
+    },
 
-const [person, setPerson] = useState({name: 'Collins', age: 18})
+    {
+      color: "Navy Blue",
+      type: "Honda",
+      registration: new Date("2021-11-10"),
+      capacity: 32,
+    },
+    {
+      color: "Purple",
+      type: "Bike",
+      registration: new Date("2018-03-03"),
+      capacity: 19,
+    },
+  ]);
 
-  const clickHandler = () => {
-    
-    setPerson({name: 'Kiyie', age: 19})
+
+  const pressHandler = (id) => {
+    alert(id);
   }
 
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>User: {person.name} is {person.age} old</Text>
-      </View>
-
-      <View style={styles.body}>
-        <Text style={styles.boldText}>What is React native</Text>
-        <Text>
-          React is a framework and a library for interacting with data and data
-          management. React is used for a wide variety of types of tasks, from
-          the UI to state management, to data management. Why is React cool? React is
-          the first React framework, it is the first framework that can create
-          high performance, robust and complete services that are in short
-          supply in the most difficult scenarios. A framework can be created
-          because its developer community could help define and test it.
-        </Text>
-      </View>
-
-      <View style={styles.buttonContainer}><Button onPress={()=>{clickHandler()}} title='Update user name'/></View>
-    </View>
+    // Using FlatList
+      <FlatList 
+      numColumns={2}
+      keyExtractor={(item)=> item.capacity}
+      data={cars}
+      renderItem={(({item})=>{
+       return(
+        <TouchableOpacity style={{ border:1 }} onPress={()=>{pressHandler(item.capacity)}}>
+          <Text style={styles.item}>
+          {item.type}
+         </Text>
+        </TouchableOpacity>
+       )
+      })}
+      />
+     
+    // Using loops
+    // <ScrollView>
+    //   <View style={styles.container}>
+    //     {cars.map((car) => (
+    //       <View key={car.capacity} style={styles.item}>
+    //         <Text>{car.type}</Text>
+    //         <Text>{car.color}</Text>
+    //       </View>
+    //     ))}
+    //   </View>
+    // </ScrollView>
   );
-}
+};
 
-export default  App;
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  header: {
-    backgroundColor: "#f1632a",
+  item: {
+    marginTop: 24,
     padding: 20,
+    backgroundColor: "orange",
+    fontSize: 24,
+    borderRadius: 8,
+    marginHorizontal: 5,
+    
   },
-  boldText: {
-    fontWeight: "bold",
-  },
-  body: {
-    backgroundColor: "#f3f4f7",
-    padding: 20,
-  },
-  buttonContainer:{
-    borderRadius: 50,
-    padding: 20,
-    borderColor: '#f3f4f7',
-   
-  }
 });
-
-/* <Text>Medium List</Text>
-      <StatusBar style="auto" /> */
